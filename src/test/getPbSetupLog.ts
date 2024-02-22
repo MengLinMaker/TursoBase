8413626b047eaa4ff9120b92ad66c55ac10bd0b4
Meng Lin <menglinmaker@gmail.com>
2024-02-23 09:33:29 +1100

    Test: obtain PocketBase setup logs

---
+++ src/test/getPbSetupLog.ts
@@ -0,0 +1,26 @@
+import { spawn } from "node:child_process"
+import { delay } from "./delay"
+
+export const getPbSetupLog = async (databaseDir: string, port: number) => {
+  let pbStdout = ""
+  let pbServed = false // Do not update database before setup
+
+  const pbProcess = spawn("pocketbase", [
+    "serve",
+    "--dev",
+    `--http="localhost:${port}"`,
+    `--dir=${databaseDir}`,
+  ])
+  pbProcess.stdout.on("data", async (stdout: Buffer) => {
+    pbStdout += stdout.toString()
+    if (pbStdout.includes("http://")) {
+      pbServed = true
+      spawn("sqlite3", [`${databaseDir}/data.db`, "VACUUM"])
+      spawn("sqlite3", [`${databaseDir}/data.db`, "VACUUM"])
+      pbProcess.kill()
+    }
+  })
+  while (pbServed == false) await delay(100)
+  for (let i = 0; i < 3; i++) {}
+  return pbStdout
+}
