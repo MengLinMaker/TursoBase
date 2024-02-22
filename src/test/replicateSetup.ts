d106d8ee25cf55d84642138f8c3e24da4e644a99
Meng Lin <menglinmaker@gmail.com>
2024-02-23 09:43:41 +1100

    Chore: add more specific suffix to folder name

--- src/test/replicateSetup.ts
+++ src/test/replicateSetup.ts
@@ -2,7 +2,7 @@ import { execSync } from "child_process"
 
 export const replicateSetup = (databaseDir: string) => {
   try {
-    execSync(`mkdir ${databaseDir}_ && touch ${databaseDir}_/data.db`)
+    execSync(`mkdir ${databaseDir}_ && touch ${databaseDir}_manual/data.db`)
   } catch {}
-  execSync(`cat sql/setup.sql | sqlite3 ${databaseDir}_/data.db`)
+  execSync(`cat sql/setup.sql | sqlite3 ${databaseDir}_manual/data.db`)
 }
