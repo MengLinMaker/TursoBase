2eeabd6df617c10f5e25ea85077ba09f39558ba3
Meng Lin <menglinmaker@gmail.com>
2024-02-22 19:03:30 +1100

    Chore: prettier

--- src/format/pbQueryFormatter.local.test.ts
+++ src/format/pbQueryFormatter.local.test.ts
@@ -1,17 +1,17 @@
-import { describe, expect, test } from 'vitest'
-import { readFileFormatted } from '../test/readFileFormatted'
-import { pbQueryFormatter } from './pbQueryFormatter'
+import { describe, expect, test } from "vitest"
+import { readFileFormatted } from "../test/readFileFormatted"
+import { pbQueryFormatter } from "./pbQueryFormatter"
 
-describe('pbQueryFormatter', () => {
-  test('createTable', () => {
-    const pbSql = readFileFormatted('sql/createTable.pb.sql')
-    const expectedSql = readFileFormatted('sql/createTable.sql')
+describe("pbQueryFormatter", () => {
+  test("createTable", () => {
+    const pbSql = readFileFormatted("sql/createTable.pb.sql")
+    const expectedSql = readFileFormatted("sql/createTable.sql")
     const resultSql = pbQueryFormatter(pbSql)
     expect(resultSql).toBe(expectedSql)
   })
-  test('insert', () => {
-    const pbSql = readFileFormatted('sql/insertRow.pb.sql')
-    const expectedSql = readFileFormatted('sql/insertRow.sql')
+  test("insert", () => {
+    const pbSql = readFileFormatted("sql/insertRow.pb.sql")
+    const expectedSql = readFileFormatted("sql/insertRow.sql")
     const resultSql = pbQueryFormatter(pbSql)
     expect(resultSql).toBe(expectedSql)
   })
