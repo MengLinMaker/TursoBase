671961121cdb562d95ae7a1a1d825599ca45fa3e
Meng Lin <menglinmaker@gmail.com>
2024-02-22 15:10:24 +1100

    Test: add pbQueryFormatter test

---
+++ src/format/pbQueryFormatter.local.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from 'vitest'
+import { readFileFormatted } from '../test/readFileFormatted'
+import { pbQueryFormatter } from './pbQueryFormatter'
+
+describe('pbQueryFormatter', () => {
+  test('createTable', () => {
+    const pbSql = readFileFormatted('sql/createTable.pb.sql')
+    const expectedSql = readFileFormatted('sql/createTable.sql')
+    const resultSql = pbQueryFormatter(pbSql)
+    expect(resultSql).toBe(expectedSql)
+  })
+  test('insert', () => {
+    const pbSql = readFileFormatted('sql/insertRow.pb.sql')
+    const expectedSql = readFileFormatted('sql/insertRow.sql')
+    const resultSql = pbQueryFormatter(pbSql)
+    expect(resultSql).toBe(expectedSql)
+  })
+})
